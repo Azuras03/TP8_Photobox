9f84a0e693c720dbba19dbfaa058e4daf67594e6
Hugoll-1 <hugolin.rouyer9@etu.univ-lorraine.fr>
2023-05-25 14:52:54 +0200

    Ajout du bon Index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-//import photoloader from "./lib/photoloader.js";
+import photoloader from "./lib/photoloader.js";
 
+function getPicture (id){
+    let image = loadPicture(id);
+    console.log(image.titre);
+    console.log(image.type);
+    console.log(image.url);
+
+}
 
 //const getPicture = getPicture(105);
