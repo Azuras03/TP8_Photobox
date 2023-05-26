527e82f448ff989fe1fd0410c5f8c46e8f146eca
Azuras03 <nr03russo@gmail.com>
2023-05-26 14:02:11 +0200

    Gallerie affichée/Fix

--- photobox.js
+++ photobox.js
@@ -3,9 +3,18 @@ import gallery from "./lib/gallery.js";
 
 let page = 0;
 
-let load_gallery = async function () {
-    await gallery.load(page);
-    await gallery_ui.display_gallery(gallery);
-}
+document.getElementById("load_gallery").addEventListener("click", async function () {
+    await load_gallery();
+});
+document.getElementById("next_page").addEventListener("click", async function () {
+    page++;
+    await load_gallery();
+});
+document.getElementById("previous_page").addEventListener("click", async function () {
+    page--;
+    await load_gallery();
+});
 
-load_gallery();
+let load_gallery = async function () {
+    await gallery_ui.display_gallery(await gallery.load(page));
+}
