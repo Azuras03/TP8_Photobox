7de8cc464f970886a3b3f6c47b8f7c6a74dcd9e5
Azuras03 <nr03russo@gmail.com>
2023-06-04 14:40:51 +0200

    Mise en place des boutons next, prvious, last et first

--- photobox.js
+++ photobox.js
@@ -1,20 +1,36 @@
 import gallery_ui from "./lib/gallery_ui.js";
 import gallery from "./lib/gallery.js";
 
-let page = 0;
+let pageLink = "/www/canals5/phox/api/photos/";
+
+let res = await gallery.load(pageLink);
+
+let linkNextPage = res.links.next.href;
+let linkPreviousPage = res.links.prev.href;
+let linkFirstPage = res.links.first.href;
+let linkLastPage = res.links.last.href;
+
+let load_gallery = async function (pageL) {
+    let res = await gallery_ui.display_gallery(await gallery.load(pageL));
+    pageLink = pageL;
+    linkNextPage = res.next.href;
+    linkPreviousPage = res.prev.href;
+}
 
 document.getElementById("load_gallery").addEventListener("click", async function () {
-    await load_gallery();
+    await load_gallery(pageLink);
 });
 document.getElementById("next_page").addEventListener("click", async function () {
-    page++;
-    await load_gallery();
+    await load_gallery(linkNextPage);
 });
 document.getElementById("previous_page").addEventListener("click", async function () {
-    page--;
-    await load_gallery();
+    await load_gallery(linkPreviousPage);
 });
 
-let load_gallery = async function () {
-    await gallery_ui.display_gallery(await gallery.load(page));
-}
+document.getElementById("first_page").addEventListener("click", async function () {
+    await load_gallery(linkFirstPage);
+});
+
+document.getElementById("last_page").addEventListener("click", async function () {
+    await load_gallery(linkLastPage);
+});
